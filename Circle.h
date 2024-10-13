6fdf281ca4a28991816b5835fc3e7c8cfd629a6b
Vbelmar <vicentebelenguer05@gmail.com>
2024-10-14 00:38:06 +0200

    Declaración de la clase Circle

---
+++ Circle.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Shape.h"
+
+class Circle : public Shape {
+	private:
+		Point2D center;
+		double radius;
+
+	public:
+		Circle() : center(0,0), radius(1) {};
+
+		Circle(std::string color, Point2D center, double radius);
+
+		Point2D get_center() const;
+
+		void set_center(Point2D p);
+
+		double get_radius() const;
+
+		void set_radius(double r);
+
+		friend std::ostream& operator<<(std::ostream &out, const Circle &c);
+
+		double area() const override;
+
+		double perimeter() const override;
+
+		void translate(double incX, double incY) override;
+
+		void print() override;
+
+};
